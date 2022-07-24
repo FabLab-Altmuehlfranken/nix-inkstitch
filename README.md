66259fe3bce203ce948a672cd57e9b136ec6ecaa
Rolf Pfeffertal <tropf@users.noreply.github.com>
2022-07-24 16:39:56 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,47 @@
+# Ink/Stitch for nix
+This repository is a [nix flake](https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html) for the [Ink/Stitch](https://inkstitch.org/) [Inkscape](https://inkscape.org/) extension.
+It can be used to create designs for machine embroidery.
+
+The flake provides the binary `inkscape-inkstitch`,
+where you can find Ink/Stitch in the *Extensions* menu.
+Installing this flake will **not** add Ink/Stitch to you **default** inkscape installation.
+
+Run with:
+
+```bash
+nix run git+https://codeberg.org/tropf/nix-inkstitch
+```
+
+> Parts of the software stack of Ink/Stitch are horribly outdated,
+> and this flake is *very* hacky.
+> I highly recommend to not install it permanently on your system.
+
+Color Palette installation is not supported.
+
+## Notes on Packaging
+- packages contained:
+  - `pyembroidery`: python embroidery library
+  - `inkstitch`: Ink/Stitch itself
+  - `inkscape-inkstitch`: inkscape bundled with Ink/Stitch
+- patches applied:
+  - some path search includes `/usr`-paths, this is removed
+  - `trimesh` include causes deprecation warnings, those are silenced
+  - fix JS dependencies: fix reference in `package.json`, update `yarn.lock`
+- Electron is used for some of the GUI, notably color palette installation and simulation/preview.
+  This part of the package is thrown together and rather wonky, in particular:
+  - The build process of Ink/Stitch is 1) generate `main.js` via vue-electron, then 2) package with `electron-builder`.
+    This flake generates `main.js`, but never bundles it into a standalone binary.
+    The nix-provided `electron` binary is used to launch the application.
+    For that build script and invocation are patched.
+  - As the used electron version is replaced by the one from nix which is significantly newer,
+    the newer and more secure defaults cause the GUI to break.
+    Consequently, these security features are disabled (via a patch).
+  - The port is not communicated correctly to the electron application.
+    As a dirty fix, the connection port (5000) is hardcoded.
+    (Note that it was hardcoded in the python part previously already.)
+  - During my tests the vue router ran into an infinite recursion issue.
+    This is fixed by removing all routes, which disabled palette installation.
+
+## License
+The code in this repository is licensed under [GPLv3 or later](./COPYING).
+Please note that the libraries referenced by this flake may use a different license.
---
+++ flake.nix
@@ -0,0 +1,168 @@
+{
+  description = "inkscape with inkstitch";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {system="x86_64-linux";};
+      inkstitch_version = "2.2.0";
+      inkstitch_src = pkgs.fetchzip {
+        url = "https://github.com/inkstitch/inkstitch/archive/refs/tags/v${inkstitch_version}.tar.gz";
+        sha256 = "sha256-WitR7WDReDSABlDfzwCrNuqxniv0JWRlkn80zvUuq4U=";
+      };
+      inkstitch_panzoom_src = pkgs.fetchgit {
+        url = "https://github.com/inkstitch/svg.panzoom.js";
+        rev = "98c25f32925ac62f49b81d3054d99b8c183afad8";
+        sha256 = "";
+        fetchSubmodules = true;
+        deepClone = true;
+        leaveDotGit = true;
+      };
+      inkstitch_src_patched_yarn_deps = pkgs.applyPatches {
+        src = inkstitch_src;
+        patches = [ ./patches/update_js_deps.patch ];
+      };
+      inkstitch_electron_yarn_modules = pkgs.mkYarnPackage rec {
+        pname = "inkstitch-yarn-deps";
+        version = "${inkstitch_version}";
+        src = "${inkstitch_src_patched_yarn_deps}/electron";
+        packageJSON = "${inkstitch_src_patched_yarn_deps}/electron/package.json";
+        yarnLock = "${inkstitch_src_patched_yarn_deps}/electron/yarn.lock";
+      };
+      inkstitch_python_env = given_pyembroidery: pkgs.python3.withPackages (ps: [
+        # inkstitch-owned python module -- must be given as argument!!
+        given_pyembroidery
+
+        # copied by hand from requirements.txt
+        ps.backports_functools_lru_cache
+        ps.inkex
+        ps.wxPython_4_1
+        ps.networkx
+        ps.shapely
+        ps.lxml
+        ps.appdirs
+        ps.numpy
+        ps.jinja2
+        ps.requests
+        ps.colormath
+        ps.stringcase
+        ps.tinycss2
+        ps.flask
+        ps.fonttools
+        ps.trimesh
+        ps.scipy
+        ps.six
+      ]);
+    in {
+      packages.x86_64-linux.pyembroidery = with pkgs.python3.pkgs; buildPythonPackage rec {
+        pname = "pyembroidery";
+        version = "1.4.36";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-GkEoxmdhjehd+ECrcJE7vVvRlnvk6qasO3NnCGlB/VQ=";
+        };
+        doCheck = false;
+      };
+
+      packages.x86_64-linux.inkstitch = pkgs.stdenv.mkDerivation rec {
+        pname = "inkstitch";
+        version = "${inkstitch_version}";
+        src = inkstitch_src_patched_yarn_deps;
+
+        patches = [
+          # looks for /usr/bin etc., then fails -> remove that section
+          ./patches/fix_path_correction.patch
+
+          # includes deprecated feature -> don't care
+          ./patches/trimesh_silence_deprecation_warning.patch
+
+          # update requires internet connection -> disable update before build
+          ./patches/electron_build_no_update.patch
+
+          # electorn got too many security features for inkstitch to work
+          # -> disable security features
+          ./patches/fix_electron_isolation.patch
+
+          # vue router causes infinite recursion
+          # -> remove all routes but one
+          ./patches/sledgehammer_fix_vue_router.patch
+
+          # add error message when trying to install
+          # (part of electron application, but route got disabled in patch above)
+          ./patches/disable_palette_install.patch
+
+          # vue does not correctly load the port -> hardcode it
+          ./patches/hardcode_port.patch
+          
+          # invocation: do not use yarn develop, but instead pass path to electron
+          # (yarn develop also calls vue-electron build, which we called already during installation)
+          (pkgs.substituteAll {
+            electron_full_path = "${pkgs.electron}/bin/electron";
+            src = ./patches/electron_fix_invocation.patch.template;
+          })
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          # for python dependencies, hand-copied requirements.txt
+          (inkstitch_python_env self.packages.x86_64-linux.pyembroidery)
+
+          # JS stuff
+          yarn
+          nodejs
+
+          # undocumented build dependencies
+          gettext
+          which
+        ];
+        propagatedBuildInputs = with pkgs; [
+          # pre-built yarn modules (circumvent required internet access of yarn/npm during build)
+          inkstitch_electron_yarn_modules
+
+          # used at runtime (see patches above)
+          electron
+        ];
+
+        preBuild = ''
+            ln -sf ${inkstitch_electron_yarn_modules}/libexec/inkstitch-gui/node_modules electron/node_modules
+          '';
+
+        buildPhase = ''
+            runHook preBuild
+
+            # patched version: only builds main.js, does not package into full application
+            bash bin/build-electron
+
+            make inx
+
+            runHook postBuild
+          '';
+
+        installPhase = ''
+            runHook preInstall
+
+            export INKSCAPE_PLUGIN_PATH="$out/share/inkscape/extensions"
+            mkdir -p $INKSCAPE_PLUGIN_PATH
+            cp -r . $INKSCAPE_PLUGIN_PATH/inkstitch
+            
+            runHook postInstall
+          '';
+      };
+      packages.x86_64-linux.inkscape-inkstitch = pkgs.symlinkJoin {
+        name = "inkscape-inkstitch";
+        paths = [
+          pkgs.inkscape
+          self.packages.x86_64-linux.inkstitch 
+        ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+        postBuild = ''
+            export SITE_PACKAGES=$(find "${inkstitch_python_env self.packages.x86_64-linux.pyembroidery}" -type d -name 'site-packages')
+            rm -f $out/bin/inkscape
+            makeWrapper "${pkgs.inkscape}/bin/inkscape" "$out/bin/inkscape-inkstitch" \
+              --set INKSCAPE_DATADIR "$out/share" \
+              --prefix PYTHONPATH ":" "$SITE_PACKAGES"
+          '';
+      };
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.inkscape-inkstitch;
+    };
+}
